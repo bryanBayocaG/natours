679011fc3f90e571e91fcd00b8ade64d108a7007
Bryan Bayoca <bryanbayoca686@gmail.com>
2025-04-18 16:14:18 +0800

    col-1-of-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './component/header/header.component';
+import { AboutComponent } from './component/about/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
